05b597fbd8d3cdaad284d414e6e0c9fb37ab2db1
HanYi <nghanyi1997@gmail.com>
2021-11-10 00:16:58 +0800

    #94 setup pymongo

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
+import os
 from flask import Flask
+from flask_pymongo import PyMongo
+# from db import db
 
 app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello World'
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+mongo = PyMongo(app, tlsAllowInvalidCertificates=True)
+db = mongo.db
--- carstep.py
+++ carstep.py
@@ -1,5 +1,7 @@
 from app import app
+from dotenv import load_dotenv
 
 if __name__ == "__main__":
-    # Error will be displayed on web page
+    load_dotenv()
+    # app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
     app.run(debug=True)
