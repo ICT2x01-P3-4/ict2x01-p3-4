46f6199a6a919590a68943b472f20c2d9a21fa45
HanYi <nghanyi1997@gmail.com>
2021-11-13 16:47:20 +0800

    #97 create step model

---
+++ app/models/step.py
@@ -0,0 +1,19 @@
+from ..db import db
+
+
+class Step:
+    def __init__(self, step_num, direction):
+        self.step_num = step_num
+        self.direction = direction
+
+    def get_step_num(self):
+        return self.step_num
+
+    def set_step_num(self, step_num):
+        self.step_num = step_num
+
+    def get_direction(self):
+        return self.direction
+
+    def set_direction(self, direction):
+        self.direction = direction
