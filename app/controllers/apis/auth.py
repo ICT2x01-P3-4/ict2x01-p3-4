55bb660ef79c93eef05b5dc6310a53ee54da1662
alphonsekoh <alphonsekoh@gmail.com>
2021-11-17 01:08:59 +0800

    chore: included mongodb registry in auth

--- app/controllers/apis/auth.py
+++ app/controllers/apis/auth.py
@@ -1,8 +1,15 @@
 from flask import Blueprint, app, render_template, redirect, url_for, request, flash, session
 import bcrypt
+import pymongo
+import os
 from ...models import user
 
 auth = Blueprint('admin_bp', __name__)
+
+
+client = pymongo.MongoClient(os.environ.get("MONGO_URI"))
+db = client.get_database('admins')
+admins = db.register
 
 
 @auth.route('/login', methods=['POST', 'GET'])
