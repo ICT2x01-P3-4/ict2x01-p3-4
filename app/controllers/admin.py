91f6bcd8071f85e3815fbf835d53587a19e6d660
alphonsekoh <alphonsekoh@gmail.com>
2021-11-17 14:32:52 +0800

    chore: shifted login to route and added logout functionality

--- app/controllers/admin.py
+++ app/controllers/admin.py
@@ -1,4 +1,6 @@
-from flask import render_template
+from flask import Blueprint, app, render_template, redirect, url_for, request, flash, session
+import bcrypt
+from ..db import mongo
 
 
 def index():
@@ -22,4 +24,58 @@ def create_puzzle():
 
 
 def login():
-    render_template('admin/login.html')
+    '''
+    API to authenticate user from the database.
+    The authentication does a HTTP GET request method to get 
+    the information the user typed in, and does a POST request to the database to 
+    verify the user.
+
+    Add-on features: bcrypt encryption for password hashing.  
+    '''
+
+    admins = mongo.db.users
+    message = 'Please login to your account'
+    # if already logged in, start session
+    if "username" in session:
+        return redirect(url_for("login"))
+
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        user_found = admins.findone({"username": username})
+        admin_verify = admins.find_one({"role": "admin"})
+
+        # if username is found inside the database
+        if user_found:
+            if admin_verify:
+                user_valid = user_found['username']
+                password_check = user_found['password']
+
+                # compare hashed password in db with password typed
+                if bcrypt.checkpw(password.encode('utf-8'), password_check):
+                    session["username"] = user_valid
+                    return redirect(url_for("index"))
+
+                else:
+                    if "username" in session:
+                        return redirect(url_for("index"))
+                    message = 'Wrong Password'
+                    return render_template('admin/login.html', message=message)
+            else:
+                message = "You are not an admin"
+                return render_template('admin/login.html', message=message)
+        else:
+            message = 'Username is not found'
+            return render_template('admin/login.html', message=message)
+        return render_template('admin/login.html', message=message)
+
+
+def logout():
+    '''
+    Destroys the session the user is in
+    and redirects back to index back. 
+    '''
+    if "username" in session:
+        session.pop("username", None)
+    render_template("admin/index.html")
