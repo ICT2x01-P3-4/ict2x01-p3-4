6e7fc53029139c0d368ca2d2c2d062bdb870bce8
alphonsekoh <alphonsekoh@gmail.com>
2021-11-18 22:37:50 +0800

    chore: update execute_steps

--- app/controllers/apis/freestyle.py
+++ app/controllers/apis/freestyle.py
@@ -8,7 +8,7 @@ from flask import jsonify, request, session
 
 def check_steps(steps: List, STEP_LIMIT: int) -> bool:
     '''
-    Check the amount of steps 
+    Check the amount of steps satisfies the step limit. 
 
     Args:
         steps (list): list of steps from the robotic car
@@ -34,19 +34,24 @@ def execute_steps(steps: List) -> bool:
     Returns:
         boolean: True when queue is emptied.
     """
-    # Format steps into list of dicts
-    steps_arr_obj = []
-    for i, step in enumerate(steps):
-        steps_arr_obj.append({"step_num": i+1, "direction": step})
+    if check_steps(steps, 15) == True:
+        # Format steps into list of dicts
+        steps_arr_obj = []
+        for i, step in enumerate(steps):
+            steps_arr_obj.append({"step_num": i+1, "direction": step})
 
-    # Insert steps into queue
-    step = Step()
-    step.create_queue(steps_arr_obj)
+        # Insert steps into queue
+        step = Step()
+        step.create_queue(steps_arr_obj)
 
-    # Wait for queue to be emptied
-    queue_flag = True
-    while queue_flag == True:
-        if step.get_queue_count() == 0:
-            queue_flag = False
+        # Wait for queue to be emptied
+        queue_flag = True
+        while queue_flag == True:
+            if step.get_queue_count() == 0:
+                queue_flag = False
 
-    return True
+        return True
+
+    else:
+
+        return False
