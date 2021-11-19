70ac1bd5d065bf09db4cf5b87cabb2b293058e1d
HanYi <nghanyi1997@gmail.com>
2021-11-20 00:27:05 +0800

    Remove db import

--- app/models/step.py
+++ app/models/step.py
@@ -1,12 +1,8 @@
-from ..db import mongo
-
-
 class Step:
     def __init__(self, step=None):
         if step is not None:
             self.step_num = step["step_num"]
             self.direction = step["direction"]
-        self.step_queue_db = mongo.db.step_queue
 
     def get_step_num(self):
         return self.step_num
