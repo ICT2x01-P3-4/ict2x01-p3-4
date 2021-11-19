15812726edd4130e0e6708956e77d124cc209e1f
HanYi <nghanyi1997@gmail.com>
2021-11-20 00:13:12 +0800

    Remove api folder from controller

--- app/controllers/apis/car.py
+++
@@ -1,8 +0,0 @@
-from ...models.step import Step
-
-
-def get_command():
-    step = Step()
-    command = step.get_next_step()
-    direction = command["direction"]
-    return f"Command: {direction}\0"
