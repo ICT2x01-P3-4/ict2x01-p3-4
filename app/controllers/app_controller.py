9e4fa8e4b2198b429e71543eafbeeb97f922b911
alphonsekoh <alphonsekoh@gmail.com>
2021-11-20 20:42:02 +0800

    chore: app controller for user session

--- app/controllers/app_controller.py
+++ app/controllers/app_controller.py
@@ -1,3 +1,14 @@
-from flask import render_template
+from flask import render_template, redirect, url_for, request, session
+from ..models.user_model import UserModel
+
+
 def index():
-    return render_template("index.html")
+    if 'name' in session:
+        return redirect(url_for("game_mode"))
+    user = UserModel()
+    name = request.form.get('name')
+    in_session = user.login_user(name)
+
+    if not in_session:
+        return render_template("index.html")
+    return redirect(url_for("game_mode"))
