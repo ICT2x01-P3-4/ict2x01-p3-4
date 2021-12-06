f434adf704964f084510e18eae049e7e6da7ff5b
alphonsekoh <alphonsekoh@gmail.com>
2021-12-06 16:00:10 +0800

    chore: diff config for test and prod

--- app/__init__.py
+++ app/__init__.py
@@ -8,23 +8,25 @@ from .routes.api_bp import api_bp
 from .routes.admin_bp import admin_bp
 from flask_session import Session
 
-# Create Flask app and initialize mongodb connection
-app = Flask(__name__)
-app.secret_key = os.environ.get("SECRET_KEY")
-app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
-app.config['SESSION_PERMANENT'] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
-mongo.init_app(app, tlsAllowInvalidCertificates=True)
+def get_app_with_config(config):
+    # Create Flask app and initialize mongodb connection
+    app = Flask(__name__)
+    app.secret_key = os.environ.get("SECRET_KEY")
+    app.config.from_object(config)
+    app.config['SESSION_PERMANENT'] = False
+    app.config["SESSION_TYPE"] = "filesystem"
+    Session(app)
+    mongodb = mongo.init_app(app, tlsAllowInvalidCertificates=True)
 
-seed_data()
+    seed_data()
 
-# Register blueprints
-app.register_blueprint(app_bp, url_prefix="/")
-app.register_blueprint(api_bp, url_prefix="/api")
-app.register_blueprint(admin_bp, url_prefix="/admin")
+    # Register blueprints
+    app.register_blueprint(app_bp, url_prefix="/")
+    app.register_blueprint(api_bp, url_prefix="/api")
+    app.register_blueprint(admin_bp, url_prefix="/admin")
 
 
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template("404.html"), 404
+    @app.errorhandler(404)
+    def page_not_found(e):
+        return render_template("404.html"), 404
+    return app, mongodb
--- carstep.py
+++ carstep.py
@@ -1,6 +1,8 @@
-from app import app
+from app import get_app_with_config
 from dotenv import load_dotenv
+from config import RunConfig
 
+app, mongodb = get_app_with_config(RunConfig)
 if __name__ == "__main__":
     load_dotenv()
     app.run(debug=True)
