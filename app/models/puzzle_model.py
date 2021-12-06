f8b108034302fdb6b3dfcfeabb656c03f2670bf8
alphonsekoh <alphonsekoh@gmail.com>
2021-12-07 02:38:18 +0800

    fix: removed unncessary if statement

--- app/models/puzzle_model.py
+++ app/models/puzzle_model.py
@@ -102,8 +102,7 @@ class PuzzleModel:
             object: puzzle object from the database.
         """
         puzzle = self.puzzle_db.find_one({"_id": ObjectId(id)})
-        if puzzle:
-            puzzle["_id"] = str(puzzle["_id"])
+        puzzle["_id"] = str(puzzle["_id"])
         return puzzle
 
     def get_puzzle_by_stage(self, stage):
